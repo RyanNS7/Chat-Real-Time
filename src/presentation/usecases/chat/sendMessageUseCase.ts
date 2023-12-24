9c85cdc71c1fad8e31f69d13e49812e35ee8a584
Ryanns7 <ryanns777@gmail.com>
2023-12-24 03:37:49 -0300

    change in the values that will be returned

--- src/presentation/usecases/chat/sendMessageUseCase.ts
+++ src/presentation/usecases/chat/sendMessageUseCase.ts
@@ -1,4 +1,5 @@
 import { messages } from "../../../domain/entities/chat/messages";
+import { verificationStatus } from "../../../domain/entities/verificationStatus";
 import { sendMessages } from "../../../domain/usecases/chat/sendMessages"
 import { userRepository } from "../../../domain/usecases/user/userRepository";
 
@@ -11,24 +12,34 @@ export class SendMessageUseCase implements sendMessages{
         this.userRepository = userRepository
     }
 
-    async send(userMessage: messages): Promise<Error | messages>{
+    async send(userMessage: messages): Promise<verificationStatus | messages>{
 
-        if(await this.userRepository.exits(userMessage.id_User) === false){
-            return new Error("User not exists")
+        if((await this.userRepository.exits(userMessage.id_User)).status === false){
+            return {
+                error: "User not exists",
+                status: false
+            }
         }
 
         if(userMessage.message.length < 1){
-            return new Error("Cannot send empty space")
+            return {
+                error: "Cannot send empty space",
+                status: false
+            }
         }
 
         if(userMessage.message.length > 100){
-            return new Error("Too many characters typed")
+            return {
+                error: "Too many characters typed",
+                status: false
+            }
         }
 
         return {
             message: userMessage.message,
             id_User: userMessage.id_User,
-            sending_time: userMessage.sending_time
+            sending_time: userMessage.sending_time,
+            status: true
         }
     }
 
