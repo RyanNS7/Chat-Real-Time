ba4bb9ee66de214d73177d879b3bb47a7aa908c1
Ryanns7 <ryanns777@gmail.com>
2023-12-28 12:22:50 -0300

    jest settings using typescript

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {Config} from 'jest';
+
+const config: Config = {
+      testEnvironment: "node",
+      testPathIgnorePatterns: ['/node_modules/'],
+      collectCoverage: true,
+      collectCoverageFrom: ['src/**/*.ts(x)', 'src/**/*spec.ts(x)'],
+      bail: true,
+      clearMocks: true,
+};
+
+export default config;
