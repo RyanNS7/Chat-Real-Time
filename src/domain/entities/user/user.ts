3a767721ec1ab7ad18f4d8d4feca290c32a9b3f3
Ryanns7 <ryanns777@gmail.com>
2024-01-03 11:57:06 -0300

    creation function within the User class

--- src/domain/entities/user/user.ts
+++ src/domain/entities/user/user.ts
@@ -1,14 +1,38 @@
+import { verificationStatus } from "../verificationStatus"
+import { randomUUID } from "crypto"
+
 export class User {
-    id: string
+    readonly id: string
     name: string
     email: string
     password: string
 
-    constructor(id: string, name: string, email: string, password: string){
-        this.id = id
+    constructor(name: string, email: string, password: string){
+        this.id = randomUUID()
         this.name = name
         this.email = email
         this.password = password
+    }
+
+    static create(name: string, email: string, password: string): verificationStatus{
+        if(name.length < 3 || name.length > 40){
+            return {
+                error: "Name must be more than 3 characters and less than 40 required",
+                status: false
+            }
+        }
+
+        if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)){
+            return {
+                error: "Invalid Email",
+                status: false
+            }
+        }
+
+        return {
+            data: new User(name, email, password ),
+            status: true
+        }
     }
 
 }
