4f2a231c6381519f174644a40e1eee860f4aced6
Ryanns7 <ryanns777@gmail.com>
2024-01-05 11:43:05 -0300

    Basic changes to the variable name for better understanding when reading

--- src/tests/domain/entities/user/user.spec.ts
+++ src/tests/domain/entities/user/user.spec.ts
@@ -2,23 +2,23 @@ import { User } from "../../../../domain/entities/user/user"
 
 describe("Testing User creation", () => {
     it("should be creation user sucessfully", () => {
-        const user = User.create("Ryan", "user_Test@test.com", "Password123")
+        const sut = User.create("Ryan", "user_Test@test.com", "Password123")
 
-        expect(user).toMatchObject({status: true})
-        expect(user.data).toBeDefined()
+        expect(sut).toMatchObject({status: true})
+        expect(sut.data).toBeDefined()
     })
 
     it("should be there is an error creation username", () => {
-        const user = User.create("", "user_Test@test.com", "Password123")
+        const sut = User.create("", "user_Test@test.com", "Password123")
 
-        expect(user).toMatchObject({status: false})
-        expect(user).toMatchObject({error: "Name must be more than 3 characters and less than 40 required"})
+        expect(sut).toMatchObject({status: false})
+        expect(sut).toMatchObject({error: "Name must be more than 3 characters and less than 40 required"})
     })
 
     it("should be there is an error creating email", () => {
-        const user = User.create("Ryan", "user_Test@", "Password123")
+        const sut = User.create("Ryan", "user_Test@", "Password123")
 
-        expect(user).toMatchObject({status: false})
-        expect(user).toMatchObject({error: "Invalid Email"})
+        expect(sut).toMatchObject({status: false})
+        expect(sut).toMatchObject({error: "Invalid Email"})
     })
 })
