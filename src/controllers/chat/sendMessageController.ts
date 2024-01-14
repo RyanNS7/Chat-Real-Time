0ad3cfc9ee3d5c516ebeef0cf48e409c17c4134e
Ryanns7 <ryanns777@gmail.com>
2024-01-14 12:56:36 -0300

    creation of the user message sending controller

--- src/controllers/chat/sendMessageController.ts
+++ src/controllers/chat/sendMessageController.ts
@@ -1,17 +1,51 @@
 import { httpRequest, httpResponse } from "../../http/http";
-import { badRequest, serverError } from "../../http/statusCode/statusCode";
-
+import { badRequest, ok, serverError } from "../../http/statusCode/statusCode";
+import { SendMessageUseCase } from "../../presentation/usecases/chat/sendMessageUseCase";
+import { userRepository } from "../../domain/usecases/user/userRepository";
 
 export class sendMessageController {
-    async handle(httpRequest: httpRequest): Promise<httpResponse>{
+
+    userProps: userRepository
+
+    constructor(userProps: userRepository){
+        this.userProps = userProps
+    }
+
+    async sendMessage(httpRequest: httpRequest): Promise<httpResponse>{
 
         try {
             if(!httpRequest.body){
-                return badRequest(new Error("Body Not Found"))
+                return badRequest("Body Not Found", false)
+            }
+
+            if(!httpRequest.body.message){
+                return badRequest("Message Not Found", false)
+            }
+
+            if(!httpRequest.body.id_User){
+                return badRequest("User ID Not Found", false)
             }
         } catch (error) {
-            return serverError("Server Error")
+            return serverError("Server Error", false)
         }
+
+        if(await !this.userProps.exits(httpRequest.body.id_User)){
+            return badRequest("User does not exist", false)
+        }
+
+        const message = {
+            message: httpRequest.body.message,
+            id_User: httpRequest.body.id_User,
+            sending_time: Date.now()
+        }
+
+        const sendingMessage = await new SendMessageUseCase(this.userProps).send(message)
+
+        if((sendingMessage.status === false)){
+            return badRequest(new Error(sendingMessage.error).message, sendingMessage.status)
+        }
+
+        return ok(sendingMessage.data, true)
 
     }
 }
