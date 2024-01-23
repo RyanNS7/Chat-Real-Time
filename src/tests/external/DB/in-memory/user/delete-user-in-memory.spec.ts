0233e4b2470c077a7c071721e852f5934a17bac7
Ryanns7 <ryanns777@gmail.com>
2024-01-23 13:26:55 -0300

    testing delete user in memory database

---
+++ src/tests/external/DB/in-memory/user/delete-user-in-memory.spec.ts
@@ -0,0 +1,34 @@
+import { createUserInMemory } from "../../../../../external/DB/in-memory/user/create-user-in-memory"
+import { userRepository } from "../../../../../domain/usecases/user/userRepository"
+import { deleteUserInMemory } from "../../../../../external/DB/in-memory/user/delete-user-in-memory"
+import { systemUsers } from "../../../../../external/DB/in-memory/user/db-in-memory"
+
+async function creatingUser(name: string, email:string, password: string){
+
+    const userRepositoryMock: jest.Mocked<userRepository> = {
+        exits: jest.fn()
+    }
+
+    return await new createUserInMemory(userRepositoryMock).create({name, email, password})
+}
+
+describe("Delete user in in-memory database", () => {
+    it("should be delete user in the in-memory database", async () => {
+        
+        const user = await creatingUser("Ryan", "Test@gmail.com", "Password123")
+
+        const sut = await deleteUserInMemory.delete(user.data.body.data.id)
+
+        expect(sut).toMatchObject({status: true})
+    })
+
+    it("should be error when deleting the user because it was not found", async () => {
+        
+        const user = await creatingUser("Ryan", "Test@gmail.com", "Password")
+
+        const sut = await deleteUserInMemory.delete(user as any)
+
+        expect(sut).toMatchObject({status: false})
+        expect(sut).toMatchObject({error: "User not find"})
+    })
+})
