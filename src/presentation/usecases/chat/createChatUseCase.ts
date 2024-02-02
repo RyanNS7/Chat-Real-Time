e3dd242e072669a86768c8ca71faa1eae25ee100
Ryanns7 <ryanns777@gmail.com>
2024-02-02 13:24:47 -0300

    removing imports that were not used

--- src/presentation/usecases/chat/createChatUseCase.ts
+++ src/presentation/usecases/chat/createChatUseCase.ts
@@ -1,5 +1,4 @@
 import { chat } from "../../../domain/entities/chat/chat"
-import { messages } from "../../../domain/entities/chat/messages"
 import { verificationStatus } from "../../../domain/entities/verificationStatus"
 import { createChat } from "../../../domain/usecases/chat/createChat"
 import { userRepository } from "../../../domain/usecases/user/userRepository"
