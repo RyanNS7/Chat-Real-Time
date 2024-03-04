79707ac6a63472acb85bd6736b7506a58be03cb2
Ryanns7 <ryanns777@gmail.com>
2024-03-04 10:44:25 -0300

    testing send message use case

--- src/tests/presentation/chat/sendMessageUseCase.spec.ts
+++ src/tests/presentation/chat/sendMessageUseCase.spec.ts
@@ -1,7 +1,6 @@
 import { User } from "../../../domain/entities/user/user";
 import { SendMessageUseCase } from "../../../presentation/usecases/chat/sendMessageUseCase";
 import { messages } from "../../../domain/entities/chat/messages";
-import { sendMessages } from "../../../domain/usecases/chat/sendMessages";
 import { userRepository } from "../../../domain/usecases/user/userRepository";
 
 const user = User.create("Ryan", "user_Test@test.com", "Password123")
@@ -12,10 +11,6 @@ function userMessage(message: string, id_User: string): messages{
         id_User,
         sending_time: Date.now()
     }
-}
-
-const propsMessagesMock: jest.Mocked<sendMessages> = {
-    send: jest.fn()
 }
 
 const userRepositoryMock: jest.Mocked<userRepository> = {
@@ -32,7 +27,7 @@ describe("Send Messages", () => {
         const message = userMessage("Testando se a mensagem esta indo mesmo", user.data.id)
 
         await showingThatUserExists()
-        const sut = await new SendMessageUseCase(propsMessagesMock, userRepositoryMock).send(message)
+        const sut = await new SendMessageUseCase(userRepositoryMock).send(message)
 
         expect(sut).toMatchObject({status: true})
     })
@@ -42,7 +37,7 @@ describe("Send Messages", () => {
         const message = userMessage("", user.data.id)
 
         await showingThatUserExists()
-        const sut = await new SendMessageUseCase(propsMessagesMock, userRepositoryMock).send(message)
+        const sut = await new SendMessageUseCase(userRepositoryMock).send(message)
 
         expect(sut).toMatchObject({status: false})
         expect(sut).toMatchObject({error: 'Cannot send empty space'})
@@ -54,7 +49,7 @@ describe("Send Messages", () => {
             user.data.id)
 
         await showingThatUserExists()
-        const sut = await new SendMessageUseCase(propsMessagesMock, userRepositoryMock).send(message)
+        const sut = await new SendMessageUseCase(userRepositoryMock).send(message)
 
         expect(sut).toMatchObject({status: false})
         expect(sut).toMatchObject({error: 'Too many characters typed'})
@@ -65,7 +60,7 @@ describe("Send Messages", () => {
         await userRepositoryMock.exits.mockResolvedValue({status: false})
 
         const message = userMessage("Pelo visto esqueceu de passar as informações corretas do usuario", user.data)
-        const sut = await new SendMessageUseCase(propsMessagesMock, userRepositoryMock).send(message)
+        const sut = await new SendMessageUseCase(userRepositoryMock).send(message)
 
         expect(sut).toMatchObject({status: false})
         expect(sut).toMatchObject({error: 'User not exists'})
