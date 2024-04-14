052c03cf62f57a81c9e6b330c1d78386e81ec545
Ryanns7 <ryanns777@gmail.com>
2024-04-14 11:35:38 -0300

    database configuration

---
+++ src/external/DB/postgreSQL/config/configDB.ts
@@ -0,0 +1,8 @@
+
+export type config = {
+    host?: string | undefined
+    user?: string | undefined
+    password?: string | undefined
+    port?: number | undefined
+    connectionString?: string | undefined
+}
