c7dce68e9ad3ab1fc89c1c355aadb1b2ef8fedcb
Ryanns7 <ryanns777@gmail.com>
2024-04-20 12:42:52 -0300

    find user with unique id

---
+++ src/external/DB/postgreSQL/user/uniqueID/findWithUniqueID.ts
@@ -0,0 +1,34 @@
+import { httpResponse } from "../../../../../http/http";
+import { badRequest, ok } from "../../../../../http/statusCode/statusCode";
+import { ClientDB } from "../../config/clientDB";
+import { config } from "../../config/configDB";
+import { findUser } from "../findUser";
+
+export class findWithUniqueID{
+
+    config: config
+
+    constructor(config: config){
+        this.config = config
+    }
+
+    async find(uniqueID: string): Promise<httpResponse>{
+
+        const client = await new ClientDB(this.config).connection()
+
+        const query = {
+            text: `SELECT * FROM uniqueid WHERE id_unique = $1`,
+            values: [uniqueID]
+        }
+        
+        const find_user_unique_ID = await client.query(query)
+
+        if(find_user_unique_ID.rowCount === 0){
+            return badRequest("Unique ID not found", false)
+        }
+
+        return ok(find_user_unique_ID, true)
+
+    }
+
+}
