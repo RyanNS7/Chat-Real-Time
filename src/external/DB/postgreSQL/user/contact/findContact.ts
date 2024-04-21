826e87ca010f678958efe2dc4c44e9d471717920
Ryanns7 <ryanns777@gmail.com>
2024-04-21 13:04:13 -0300

    find contact in database

---
+++ src/external/DB/postgreSQL/user/contact/findContact.ts
@@ -0,0 +1,46 @@
+import { httpResponse } from "../../../../../http/http";
+import { badRequest, ok } from "../../../../../http/statusCode/statusCode";
+import { ClientDB } from "../../config/clientDB";
+import { config } from "../../config/configDB";
+import { findWithUniqueID } from "../uniqueID/findWithUniqueID";
+
+export class findContact {
+
+    config: config
+
+    constructor(config: config){
+        this.config = config
+    }
+
+
+    async find(uniqueID_user: string, uniqueID_contact: string): Promise<httpResponse>{
+
+        const client = await new ClientDB(this.config).connection()
+
+        const find_user = await new findWithUniqueID(this.config).find(uniqueID_user)
+        const find_contact = await new findWithUniqueID(this.config).find(uniqueID_contact)
+
+        if(find_user.statusCode === 400){
+            return badRequest(find_user.body.error, false)
+        }
+
+        if(find_contact.statusCode === 400){
+            return badRequest(find_contact.body.error, false)
+        }
+
+        const query = {
+            text: `SELECT * FROM "contact" WHERE id_user = $1 AND id_unique_contact = $2`,
+            values: [find_user.body.data.rows[0].id_user, find_contact.body.data.rows[0].id_unique]
+        }
+
+        const findContact = await client.query(query)
+
+        if(findContact.rowCount === 0){
+            return badRequest("Error when trying to find the user's contact", false)
+        }
+
+        return ok(findContact, true)
+        
+    }
+
+}
