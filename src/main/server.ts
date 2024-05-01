534fde089ed716ce811a0664d7cda48a5a7de3c6
Ryanns7 <ryanns777@gmail.com>
2024-05-01 11:45:12 -0300

    returning the value from the server to be able to add it to the webSocket server

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,8 +2,9 @@ import { Express } from "express"
 
 export function initServer(app: Express){
 
-    app.listen(process.env.SERVER_PORT || 8080, () => {
+    const server = app.listen(process.env.SERVER_PORT || 8080, () => {
         console.log("Servidor Iniciado com Sucesso")
-    })   
+    })
 
+    return server
 }
