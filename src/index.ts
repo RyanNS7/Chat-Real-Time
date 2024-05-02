e63b84c4bb29d9d5b063d4ac11fe41a3fae2892b
Ryanns7 <ryanns777@gmail.com>
2024-05-02 12:57:24 -0300

    index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
-import express from "express"
+import { config } from "./external/DB/postgreSQL/config/configDB"
+import { connectionSocket } from "./external/socket/connectionSocket"
+import { App } from "./main/app"
+import { parses } from "./main/parser"
+import { routes } from "./main/routes"
+import { initServer } from "./main/server"
+import { Router } from "express"
 
-const app = express()
+const configDB: config = {
+    connectionString: process.env.CONNECTION_STRING_DATABASE
+}
 
+const app = App()
+const router = Router()
 
+parses(app)
 
-app.listen(3333, (): void => {
-    console.log("Servidor Iniciado com Sucesso")
-})
+routes(router, configDB )
+
+const server = initServer(app)
+
+const io = connectionSocket.on(server)
+
+export {io}
